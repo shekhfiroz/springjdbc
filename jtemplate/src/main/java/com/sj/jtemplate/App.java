4285554a0ab6381498c49c2057322e3c4a88666c
shekhfirozalam <firozhere@gmail.com>
2018-07-03 00:24:49 +0530

    jdbc_template

---
+++ jtemplate/src/main/java/com/sj/jtemplate/App.java
@@ -0,0 +1,13 @@
+package com.sj.jtemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
