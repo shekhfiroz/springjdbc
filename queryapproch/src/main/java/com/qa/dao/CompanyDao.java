ceb54d74cef354dc70195ae973f8c867f1bc6af5
shekhfirozalam <firozhere@gmail.com>
2018-07-05 02:01:37 +0530

    query_approch

--- queryapproch/src/main/java/com/qa/dao/CompanyDao.java
+++ queryapproch/src/main/java/com/qa/dao/CompanyDao.java
@@ -1,19 +1,46 @@
 package com.qa.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.qa.bo.Companybo;
 
 public class CompanyDao {
 	private final String SQL_COUNT_OF_COMPANY = "select count(1) from company";
-	private final String 
+	private final String SQL_GET_COLUMN_VALUES = "select company_name from company where company_no like ?";
+	private final String SQL_GET_COMPANY_ROW = "select * from company where company_no like ?";
+
 	private JdbcTemplate jdbcTemplate;
 
 	public CompanyDao(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
-   
-	/*1=> Method to count the total nos of companies*/
+
+	/* 1=>Method to count the total nos of companies */
 	public int CountOfCompany() {
-		return jdbcTemplate.queryForObject(SQL_COUNT_OF_COMPANY,Integer.class);
+		return jdbcTemplate.queryForObject(SQL_COUNT_OF_COMPANY, Integer.class);
 	}
-	
+
+	/* 2=>Method to return companies column */
+	public String getCompanyName(int company_no) {
+		return jdbcTemplate.queryForObject(SQL_GET_COLUMN_VALUES, String.class, company_no);
+	}
+
+	/* 3=> return a complete row as an object */
+	public CompanyBo getCompanyRowObject(int company_no) {
+		
+
+		return jdbcTemplate.queryForObject(SQL_GET_COMPANY_ROW, String.class, company_no);
+
+	}
+
+	private static final class CompanyRowMapper implements RowMapper<Companybo>
+
+		@Override
+		public Companybo mapRow(ResultSet rs, int rowNum) throws SQLException {
+			return null;
+		}
 }
--- queryapproch/src/main/java/com/qa/test/QaTest.java
+++ queryapproch/src/main/java/com/qa/test/QaTest.java
@@ -9,8 +9,12 @@ public class QaTest {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/qa/common/application-context.xml");
 		CompanyDao cdao = context.getBean("companydoa", CompanyDao.class);
-		int company_count = cdao.CountOfCompany();
-		System.out.println("Total No Of Company Count => " + company_count);
+		/*
+		  int company_count = cdao.CountOfCompany();
+		  System.out.println("Total No Of Company Count => " + company_count);
+		  String returned_comapny_name=cdao.getCompanyName(3);
+		  System.out.println(returned_comapny_name);
+		 */
 	}
 
 }
