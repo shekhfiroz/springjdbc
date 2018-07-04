f6f607fb1a09757fa85bb190c4acd2b0954fc0e0
shekhfirozalam <firozhere@gmail.com>
2018-07-05 03:44:41 +0530

    inserted records into table

--- queryapproch/src/main/java/com/qa/dao/CompanyDao.java
+++ queryapproch/src/main/java/com/qa/dao/CompanyDao.java
@@ -2,6 +2,7 @@ package com.qa.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -12,9 +13,10 @@ public class CompanyDao {
 	private final String SQL_COUNT_OF_COMPANY = "select count(1) from company";
 	private final String SQL_GET_COLUMN_VALUES = "select company_name from company where company_no like ?";
 	private final String SQL_GET_COMPANY_ROW = "select * from company where company_no like ?";
-
+	private final String SQL_GET_ALL_RECORDS_OF_COMPANY = "select * from company";
+	private final String SQL_INSERT_INTO_COMPANY_TABLE="insert into company(company_no,company_name,description,established_date,location) values(?,?,?,?,?)";
 	private JdbcTemplate jdbcTemplate;
-
+	
 	public CompanyDao(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
@@ -30,17 +32,36 @@ public class CompanyDao {
 	}
 
 	/* 3=> return a complete row as an object */
-	public CompanyBo getCompanyRowObject(int company_no) {
-		
+	public Companybo getCompanyRowObject(int company_no) {
 
-		return jdbcTemplate.queryForObject(SQL_GET_COMPANY_ROW, String.class, company_no);
+		return jdbcTemplate.queryForObject(SQL_GET_COMPANY_ROW, new CompanyRowMapper(), company_no);
 
 	}
 
-	private static final class CompanyRowMapper implements RowMapper<Companybo>
+	/* inner class get 1 complete row from database and set to the java class */
+	private static final class CompanyRowMapper implements RowMapper<Companybo> {
 
 		@Override
 		public Companybo mapRow(ResultSet rs, int rowNum) throws SQLException {
-			return null;
+			Companybo cbo = new Companybo();
+			cbo.setCompany_nos(rs.getString(1));
+			cbo.setCompany_name(rs.getString(2));
+			cbo.setDescription(rs.getString(3));
+			cbo.setEstablished_Date(rs.getString(4));
+			cbo.setLocation(rs.getString(5));
+			return cbo;
 		}
+	}
+
+	/* 4=> to get multiple row of records from table */
+	public List<Companybo> getAllCompanyRecords() {
+		/* calling query() method to get all records */
+		return jdbcTemplate.query(SQL_GET_ALL_RECORDS_OF_COMPANY, new CompanyRowMapper());
+	}
+
+	/* inserting records into table */
+	public int insertrecords(int company_no,String company_name,String description,String established_date,String location) {
+		return jdbcTemplate.update(SQL_INSERT_INTO_COMPANY_TABLE,company_no,company_name,description,established_date,location);
+		
+	}
 }
--- queryapproch/src/main/java/com/qa/test/QaTest.java
+++ queryapproch/src/main/java/com/qa/test/QaTest.java
@@ -1,7 +1,12 @@
 package com.qa.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.qa.bo.Companybo;
 import com.qa.dao.CompanyDao;
 
 public class QaTest {
@@ -10,11 +15,23 @@ public class QaTest {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/qa/common/application-context.xml");
 		CompanyDao cdao = context.getBean("companydoa", CompanyDao.class);
 		/*
-		  int company_count = cdao.CountOfCompany();
-		  System.out.println("Total No Of Company Count => " + company_count);
-		  String returned_comapny_name=cdao.getCompanyName(3);
-		  System.out.println(returned_comapny_name);
+		 * int company_count = cdao.CountOfCompany();
+		 * System.out.println("Total No Of Company Count => " + company_count);
+		 * 
+		 * String returned_comapny_name=cdao.getCompanyName(3);
+		 * System.out.println(returned_comapny_name);
+		 * 
+		 * Companybo complete_rowreturned=cdao.getCompanyRowObject(2);
+		 * System.out.println(complete_rowreturned);
+		 * 
+		 * List <Companybo> allrecords =cdao.getAllCompanyRecords();
+		 * System.out.println(allrecords);
+		 * 
 		 */
+		    int recordinserted=cdao.insertrecords(4, "google internet", "internet company", "2001","california");
+		    System.out.println("Nos of records inserted =>"+recordinserted);
+		
+		
 	}
 
 }
